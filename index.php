9c6cf56bef9363bb05c4aef62cf74edd239586a2
Сергей <semyonov.serg@gmail.com>
2019-04-20 22:42:57 +0400

    Изменил вывод категорий в майне и футере

--- index.php
+++ index.php
@@ -93,13 +93,13 @@ $ad = [
         <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и горнолыжное снаряжение.</p>
         <ul class="promo__list">
             <!--заполните этот список из массива категорий-->
-            <li class="promo__item promo__item--boards">
 
-                <?php foreach ($cat as $value): ?>
+            <?php foreach ($cat as $value): ?>
+                <li class="promo__item promo__item--boards">                
                     <a class="promo__link" href="pages/all-lots.html"><?=$value;?></a>
-                <?php endforeach; ?>
+                </li>
+            <?php endforeach; ?>
 
-            </li>
         </ul>
     </section>
     <section class="lots">
@@ -108,25 +108,26 @@ $ad = [
         </div>
         <ul class="lots__list">
             <!--заполните этот список из массива с товарами-->
+
             <?php foreach ($ad as $value): ?>
-            <li class="lots__item lot">
-                <div class="lot__image">
-                    <img src=<?=$value[3];?> width="350" height="260" alt="">
-                </div>
-                <div class="lot__info">
-                    <span class="lot__category"><?=$value[1];?></span>
-                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$value[0];?></a></h3>
-                    <div class="lot__state">
-                        <div class="lot__rate">
-                            <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost"><?=$value[2];?><b class="rub">р</b></span>
-                        </div>
-                        <div class="lot__timer timer">
-                            12:23
+                <li class="lots__item lot">
+                    <div class="lot__image">
+                        <img src=<?=$value[3];?> width="350" height="260" alt="">
+                    </div>
+                    <div class="lot__info">
+                        <span class="lot__category"><?=$value[1];?></span>
+                        <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$value[0];?></a></h3>
+                        <div class="lot__state">
+                            <div class="lot__rate">
+                                <span class="lot__amount">Стартовая цена</span>
+                                <span class="lot__cost"><?=$value[2];?><b class="rub">р</b></span>
+                            </div>
+                            <div class="lot__timer timer">
+                                12:23
+                            </div>
                         </div>
                     </div>
-                </div>
-            </li>
+                </li>
             <?php endforeach; ?>
         </ul>
     </section>
@@ -137,13 +138,13 @@ $ad = [
     <nav class="nav">
         <ul class="nav__list container">
             <!--заполните этот список из массива категорий-->
-            <li class="nav__item">
 
-                <?php foreach ($cat as $value): ?>
+            <?php foreach ($cat as $value): ?>
+                <li class="nav__item">
                     <a href="pages/all-lots.html"><?=$value;?></a>
-                <?php endforeach; ?>
-
-            </li>
+                </li>
+            <?php endforeach; ?>
+            
         </ul>
     </nav>
     <div class="main-footer__bottom container">
