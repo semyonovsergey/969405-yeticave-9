fab97df24351f7ada3618912f0fd8f5006832207
Сергей <semyonov.serg@gmail.com>
2019-04-21 10:04:10 +0400

    Изменил двумерный массив товаров на ассоциативный

--- index.php
+++ index.php
@@ -4,40 +4,40 @@ $user_name = 'Semyonov Sergey'; // укажите здесь ваше имя
 $cat = ["Доски и лыжи", "Крепления", "Ботинки", "Одежда", "Инструменты", "Разное"];
 $ad = [
     [
-        "2014 Rossignol District Snowboard",
-        "Доски и лыжи",
-        10999,
-        "img/lot-1.jpg"
+        "title" => "2014 Rossignol District Snowboard",
+        "category" => "Доски и лыжи",
+        "price_begin" => 10999,
+        "pic" => "img/lot-1.jpg"
     ],
     [
-        "DC Ply Mens 2016/2017 Snowboard",
-        "Доски и лыжи",
-        159999,
-        "img/lot-2.jpg"
+        "title" => "DC Ply Mens 2016/2017 Snowboard",
+        "category" => "Доски и лыжи",
+        "price_begin" => 159999,
+        "pic" => "img/lot-2.jpg"
     ],
     [
-        "Крепления Union Contact Pro 2015 года размер L/XL",
-        "Крепления",
-        8000,
-        "img/lot-3.jpg"
+        "title" => "Крепления Union Contact Pro 2015 года размер L/XL",
+        "category" => "Крепления",
+        "price_begin" => 8000,
+        "pic" => "img/lot-3.jpg"
     ],
     [
-        "Ботинки для сноуборда DC Mutiny Charocal   ",
-        "Ботинки",
-        10999,
-        "img/lot-4.jpg"
+        "title" => "Ботинки для сноуборда DC Mutiny Charocal",
+        "category" => "Ботинки",
+        "price_begin" => 10999,
+        "pic" => "img/lot-4.jpg"
     ],
     [
-        "Куртка для сноуборда DC Mutiny Charocal    ",
-        "Одежда",
-        7500,
-        "img/lot-5.jpg"
+        "title" => "Куртка для сноуборда DC Mutiny Charocal",
+        "category" => "Одежда",
+        "price_begin" => 7500,
+        "pic" => "img/lot-5.jpg"
     ],
     [
-        "Маска Oakley Canopy    ",
-        "Разное",
-        5400,
-        "img/lot-6.jpg"
+        "title" => "Маска Oakley Canopy",
+        "category" => "Разное",
+        "price_begin" => 5400,
+        "pic" => "img/lot-6.jpg"
     ],
 ];
 ?>
@@ -109,18 +109,18 @@ $ad = [
         <ul class="lots__list">
             <!--заполните этот список из массива с товарами-->
 
-            <?php foreach ($ad as $value): ?>
+            <?php foreach ($ad as $key => $value): ?>
                 <li class="lots__item lot">
                     <div class="lot__image">
-                        <img src=<?=$value[3];?> width="350" height="260" alt="">
+                        <img src=<?=$value["pic"];?> width="350" height="260" alt="">
                     </div>
                     <div class="lot__info">
-                        <span class="lot__category"><?=$value[1];?></span>
-                        <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$value[0];?></a></h3>
+                        <span class="lot__category"><?=$value["category"];?></span>
+                        <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$value["title"];?></a></h3>
                         <div class="lot__state">
                             <div class="lot__rate">
                                 <span class="lot__amount">Стартовая цена</span>
-                                <span class="lot__cost"><?=$value[2];?><b class="rub">р</b></span>
+                                <span class="lot__cost"><?=$value["price_begin"];?><b class="rub">р</b></span>
                             </div>
                             <div class="lot__timer timer">
                                 12:23
@@ -129,6 +129,7 @@ $ad = [
                     </div>
                 </li>
             <?php endforeach; ?>
+            
         </ul>
     </section>
 </main>
