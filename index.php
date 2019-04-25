7fc7bd86202e256707af26735474895c2b5b6b0b
Сергей <semyonov.serg@gmail.com>
2019-04-25 19:59:49 +0400

    Заменил Имя страницы переменной

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ require_once("data.php");
 $page_content = include_template("index.php", ["cat" => $cat, "ad" => $ad]);
 
 $layout_content = include_template("layout.php", [
-    "page_name" => "Главная",
+    "page_name" => $page_name,
     "is_auth" => $is_auth,
     "page_content" => $page_content,
     "cat" => $cat
