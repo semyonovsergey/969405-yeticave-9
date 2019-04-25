58672bbb8090df9698321da40ef8bc09aca34bd1
Сергей <semyonov.serg@gmail.com>
2019-04-25 20:30:11 +0400

    Обернул переменные в htmlspecialchars

--- templates/index.php
+++ templates/index.php
@@ -22,11 +22,11 @@
         <?php foreach ($ad as $key => $value): ?>
             <li class="lots__item lot">
                 <div class="lot__image">
-                    <img src=<?=$value["pic"];?> width="350" height="260" alt="">
+                    <img src=<?=htmlspecialchars($value["pic"]);?> width="350" height="260" alt="">
                 </div>
                 <div class="lot__info">
                     <span class="lot__category"><?=htmlspecialchars($value["category"]);?></span>
-                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$value["title"];?></a></h3>
+                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=htmlspecialchars($value["title"]);?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
