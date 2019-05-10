5a0954e08ddd961f968c55110ed033456c3e9020
Сергей <semyonov.serg@gmail.com>
2019-05-10 20:58:41 +0400

    Добавлены длины полей, исправлено название поля, изменены типы полей

--- schema.sql
+++ schema.sql
@@ -5,43 +5,43 @@ DEFAULT COLLATE utf8_general_ci;
 USE yeticave;
 
 CREATE TABLE categories (
-id      INT AUTO_INCREMENT PRIMARY KEY,
-name    CHAR NOT NULL UNIQUE,
-code    CHAR NOT NULL UNIQUE
+id      INT(1) AUTO_INCREMENT PRIMARY KEY,
+name    CHAR(50) NOT NULL UNIQUE,
+code    CHAR(10) NOT NULL UNIQUE
 );
 
 CREATE TABLE lots (
-id      	   INT AUTO_INCREMENT PRIMARY KEY,
-data_create    TIMESTAMP,
-name    	   CHAR NOT NULL,
-description    CHAR NOT NULL,
-image		   CHAR NOT NULL,
-price_begin    DECIMAL NOT NULL,
-date_end	   TIMESTAMP NOT NULL,
-price_step     DECIMAL NOT NULL,
-author 	       CHAR REFERENCES users(name),
-winner 	       CHAR REFERENCES users(name),
-category       CHAR REFERENCES categories(name),
+id      	   INT(3) AUTO_INCREMENT PRIMARY KEY,
+date_create    DATETIME,
+name    	   CHAR(50) NOT NULL,
+description    CHAR(255) NOT NULL,
+image		   CHAR(255) NOT NULL,
+price_begin    INT(6) NOT NULL,
+date_end	   DATETIME,
+price_step     INT(6) NOT NULL,
+author 	       CHAR(20) REFERENCES users(name),
+winner 	       CHAR(20) REFERENCES users(name),
+category       CHAR(50) REFERENCES categories(name),
 INDEX(name),
 INDEX(description)
 );
 
 CREATE TABLE rates (
-id      	INT AUTO_INCREMENT PRIMARY KEY,
-date_rate   TIMESTAMP,
-price_rate	DECIMAL,
-user 		CHAR REFERENCES users(name),
-lot 		INT REFERENCES lots(id)
+id      	INT(3) AUTO_INCREMENT PRIMARY KEY,
+date_rate   DATETIME,
+price_rate	INT(6),
+user 		CHAR(20) REFERENCES users(name),
+lot 		INT(3) REFERENCES lots(id)
 );
 
 CREATE TABLE users (
-id      		INT AUTO_INCREMENT PRIMARY KEY,
-date_reg   		TIMESTAMP,
-email 			CHAR NOT NULL UNIQUE,
-name 			CHAR NOT NULL UNIQUE,
-password 		CHAR NOT NULL,
-avatar 			CHAR,
-communication	CHAR NOT NULL,
-lot 			INT REFERENCES lots(id),
-rate 			INT REFERENCES rates(id)
+id      		INT(3) AUTO_INCREMENT PRIMARY KEY,
+date_reg   		DATETIME,
+email 			CHAR(30) NOT NULL UNIQUE,
+name 			CHAR(20) NOT NULL UNIQUE,
+password 		CHAR(20) NOT NULL,
+avatar 			CHAR(255),
+communication	CHAR(30) NOT NULL,
+lot 			INT(3) REFERENCES lots(id),
+rate 			INT(3) REFERENCES rates(id)
 );
