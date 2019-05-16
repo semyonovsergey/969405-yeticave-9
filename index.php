9ff9af9ad43a846ee6261d2ca696ab42e5442848
Сергей <semyonov.serg@gmail.com>
2019-05-16 21:50:18 +0400

    Добавил условие

--- index.php
+++ index.php
@@ -4,24 +4,22 @@ require_once("helpers.php");
 require_once("data.php");
 
 $con = mysqli_connect("localhost", "root", "", "yeticave");
-if ($con == false) {
-   print("Ошибка подключения: " . mysqli_connect_error());
-}
-else {
-   print("Соединение установлено");
-}
 mysqli_set_charset($con, "utf8");
 
-$sql1 = "SELECT name, name_cat, price_begin, image FROM lots LEFT JOIN categories c ON category = c.id";
-$result1 = mysqli_query($con, $sql1);
-$ad = mysqli_fetch_all($result1, MYSQLI_ASSOC);
+if ($con) {
+    $sql = "SELECT name, name_cat, price_begin, image FROM lots LEFT JOIN categories c ON category = c.id";
+    $result = mysqli_query($con, $sql);
+    $ad = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
-$sql = "SELECT name_cat FROM categories";
-$result = mysqli_query($con, $sql);
-while ($cats = mysqli_fetch_assoc($result)) {
-    $cat[] = $cats["name_cat"];
+    $sql = "SELECT name_cat FROM categories";
+    $result = mysqli_query($con, $sql);
+    while ($cats = mysqli_fetch_assoc($result)) {
+        $cat[] = $cats["name_cat"];
+    }
 }
-
+else {
+    print("Ошибка подключения: " . mysqli_connect_error());
+}
 //var_dump($ad);
 
 $page_content = include_template("index.php", ["cat" => $cat, "ad" => $ad]);
