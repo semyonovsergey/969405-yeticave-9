c97be990a2ca8b385b4d139e13bc42b81e436e97
Сергей <semyonov.serg@gmail.com>
2019-05-17 21:00:32 +0400

    Код запроса данных из БД перенёс в отдельный файл

--- data.php
+++ data.php
@@ -5,6 +5,24 @@ date_default_timezone_set('Europe/Moscow');
 $is_auth = rand(0, 1);
 $user_name = 'Semyonov Sergey'; // укажите здесь ваше имя
 $page_name = "Главная";
+
+$con = mysqli_connect("localhost", "root", "", "yeticave");
+mysqli_set_charset($con, "utf8");
+
+if ($con) {
+    $sql = "SELECT name, name_cat, price_begin, image FROM lots LEFT JOIN categories c ON category = c.id";
+    $result = mysqli_query($con, $sql);
+    $ad = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+    $sql = "SELECT name_cat, code FROM categories";
+    $result = mysqli_query($con, $sql);
+    $cat = mysqli_fetch_all($result, MYSQLI_ASSOC);
+}
+else {
+    print("Ошибка подключения: " . mysqli_connect_error());
+}
+//var_dump($cat);
+
 //$cat = ["Доски и лыжи", "Крепления", "Ботинки", "Одежда", "Инструменты", "Разное"];
 /*
 $ad = [
--- index.php
+++ index.php
@@ -3,23 +3,6 @@
 require_once("helpers.php");
 require_once("data.php");
 
-$con = mysqli_connect("localhost", "root", "", "yeticave");
-mysqli_set_charset($con, "utf8");
-
-if ($con) {
-    $sql = "SELECT name, name_cat, price_begin, image FROM lots LEFT JOIN categories c ON category = c.id";
-    $result = mysqli_query($con, $sql);
-    $ad = mysqli_fetch_all($result, MYSQLI_ASSOC);
-
-    $sql = "SELECT name_cat, code FROM categories";
-    $result = mysqli_query($con, $sql);
-    $cat = mysqli_fetch_all($result, MYSQLI_ASSOC);
-}
-else {
-    print("Ошибка подключения: " . mysqli_connect_error());
-}
-//var_dump($cat);
-
 $page_content = include_template("index.php", ["cat" => $cat, "ad" => $ad]);
 
 $layout_content = include_template("layout.php", [
